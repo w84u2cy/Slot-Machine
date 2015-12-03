6e6cc549e5239625e1dc7c3093161510858a31ae
Ben Humphreys <benhumphreys@Bens-Mac-Pro.local>
2015-12-03 22:17:38 +1100

    first rev of project

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Ben Humphreys on 3/08/2015.
+//  Copyright (c) 2015 Ben Humphreys. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
